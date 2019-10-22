c579c9c5b9580d69239624e4ccf90f7a9090f429
Harika Mulumudi <harikamulumudi@navyas-MacBook-Pro.local>
2019-10-22 15:57:57 -0400

    added create NeighborMap call

--- tapestry/lib/insertion.ex
+++ tapestry/lib/insertion.ex
@@ -12,64 +12,47 @@ defmodule TapestrySimulator.Insertion do
     pid
   end
 
-  def insertNode(pid, nodeID) do
-    GenServer.call(pid, {:setHashId,nodeID})
+  def insertNode(pid, nodeID, table) do
+    GenServer.call(pid, {:setHashId,nodeID,table})
     GenServer.call(pid, {:setNeighbourMap,nodeID})
-     n1 = String.graphemes("356AY90B79")
+     n1 = String.graphemes("356AY78979")
      n2 = String.graphemes("356AY90B79")
-    #list =  Enum.filter(print_multiple_times(n1,n2,0), & !is_nil(&1))
-    IO.inspect (print_multiple_times(n1,n2,0))
-    #IO.inspect list[length(list)-1]
-    #IO.inspect Enum.at(list, length(list)-1)
+     len = matchSuffix(n1,n2,0)
+    IO.inspect (len)
+    
   end
 
-  def handle_call({:setHashId,nodeID}, _from ,state) do
+  def handle_call({:setHashId,nodeID,table}, _from ,state) do
    #IO.inspect state
     {_, neighborMap} = state
     hashId = :crypto.hash(:sha, Integer.to_string(nodeID))|> Base.encode16
     IO.inspect hashId
+    [{_, currentList}] = :ets.lookup(table, "count")
+    currentList = currentList ++ [hashId]
+    :ets.insert(table, {"count",currentList})
+    IO.inspect "currentList"
+    IO.inspect currentList
     state={hashId, neighborMap}
     {:reply,hashId, state}
     
   end
-  def print_multiple_times(node1, node2, n) when n >= 40 do
-    
+  def matchSuffix(node1, node2, n) when n > 9 do
+    n
   end
 
-   def print_multiple_times(node1, node2, n) do
+   def matchSuffix(node1, node2, n) do
     if(Enum.at(node1, n) == Enum.at(node2,n)) do
-      #(n+1)
-      #IO.inspect n
-      print_multiple_times(node1, node2, n + 1)
+      matchSuffix(node1, node2, n + 1)
               
           else
               
-             print_multiple_times(node1, node2, 40)
+             matchSuffix(node1, node2, 40)
              n
             end
     
   end
 
-  def matchSuffix(node1, node2) do
-   n1 = String.graphemes(node1)
-   n2 = String.graphemes(node2)
-   len = 9
-   
-     Enum.map((0..len), fn(x) ->
-          #IO.inspect Enum.at(n1, x)
-          #IO.inspect Enum.at(n2, x)
-          #IO.inspect flag
-          
-        
-             if(Enum.at(n1, x) == Enum.at(n2,x)) do
-              (x+1)
-             
-           else
-             0
-            end
-            #IO.inspect f
-        end)
-  end
+  
   def handle_call({:getHashId}, _from ,state) do
     {hashId, _} = state
     {:reply,hashId, state}
@@ -82,8 +65,31 @@ defmodule TapestrySimulator.Insertion do
 
   def handle_call({:setNeighbourMap,nodeID}, _from ,state) do
     {hashId, _} = state
+    
+    #neighborMap = Enum.map((1..40), fn(x) ->
+     #      neighborMap = %{
+      #    x => %{0 => hashId, 1 => hashId, 2 => hashId}
+       #   }
+        #end)
+
+         neighborMap = Enum.map((1..40), fn(x) ->
+           Enum.map((1..16), fn(y) ->
+           neighborMap = %{
+          x => %{y => hashId}
+          }
+          end)
+        end)
+
+        #Enum.map((1..16), fn(y) ->
+           
+       #   y => hashId
+          
+      #  end)
+
+
+        IO.inspect neighborMap
     #neighborMap =
-    #state={hashId, neighborMap}
+    state={hashId, neighborMap}
     {:reply,hashId, state}
   end
   def init(:ok) do
--- tapestry/tapestry.exs
+++ tapestry/tapestry.exs
@@ -1,14 +1,16 @@
 defmodule TapestrySimulator do
 
   def main(args) do
-    
+    table = :ets.new(:table, [:named_table,:public])
+        list = []
+        :ets.insert(table, {"count",list})
         numNodes=Enum.at(args, 0)|>String.to_integer()
 
         
 
         allNodes = Enum.map((1..numNodes), fn(x) ->
           pid=TapestrySimulator.Insertion.start_node()
-          TapestrySimulator.Insertion.insertNode(pid,x)
+          TapestrySimulator.Insertion.insertNode(pid,x,table)
           pid
         end)
 
