a4b07009fa4d30bcd096862b75f344de23d5ce1f
Harika Mulumudi <harikamulumudi@navyas-MacBook-Pro.local>
2019-10-22 00:15:13 -0400

    match suffix

--- tapestry/lib/insertion.ex
+++ tapestry/lib/insertion.ex
@@ -15,17 +15,41 @@ defmodule TapestrySimulator.Insertion do
   def insertNode(pid, nodeID) do
     GenServer.call(pid, {:setHashId,nodeID})
     GenServer.call(pid, {:setNeighbourMap,nodeID})
-
+    list =  Enum.filter(matchSuffix("356A192B79","356AYUO879"), & !is_nil(&1))
+    IO.inspect Enum.filter(matchSuffix("356A192B79","356AYUO879"), & !is_nil(&1))
+    #IO.inspect list[length(list)-1]
+    IO.inspect Enum.at(list, length(list)-1)
   end
 
   def handle_call({:setHashId,nodeID}, _from ,state) do
+   #IO.inspect state
     {_, neighborMap} = state
     hashId = :crypto.hash(:sha, Integer.to_string(nodeID))|> Base.encode16
     IO.inspect hashId
     state={hashId, neighborMap}
     {:reply,hashId, state}
+    
   end
-
+  def matchSuffix(node1, node2) do
+   n1 = String.graphemes(node1)
+   n2 = String.graphemes(node2)
+   len = 9
+   
+     Enum.map((0..len), fn(x) ->
+          #IO.inspect Enum.at(n1, x)
+          #IO.inspect Enum.at(n2, x)
+          #IO.inspect flag
+          
+        
+             if(Enum.at(n1, x) == Enum.at(n2,x)) do
+              (x+1)
+             
+           else
+             0
+            end
+            #IO.inspect f
+        end)
+  end
   def handle_call({:getHashId}, _from ,state) do
     {hashId, _} = state
     {:reply,hashId, state}
@@ -42,5 +66,7 @@ defmodule TapestrySimulator.Insertion do
     #state={hashId, neighborMap}
     {:reply,hashId, state}
   end
-
+  def init(:ok) do
+    {:ok, {0,[]}} #{hashId, neighborMap} , {hashId, neighborMap}
+  end
 end
--- tapestry/tapestry.exs
+++ tapestry/tapestry.exs
@@ -1,13 +1,10 @@
 defmodule TapestrySimulator do
 
   def main(args) do
-    if (Enum.count(args)!=2) do
-      IO.puts" Illegal Arguments Provided"
-      System.halt(1)
-    else
+    
         numNodes=Enum.at(args, 0)|>String.to_integer()
 
-        numRequests=Enum.at(args, 1)
+        
 
         allNodes = Enum.map((1..numNodes), fn(x) ->
           pid=TapestrySimulator.Insertion.start_node()
@@ -15,13 +12,15 @@ defmodule TapestrySimulator do
           pid
         end)
 
-        #infiniteLoop()
-    end
+     
   end
+   
   def infiniteLoop() do
     infiniteLoop()
   end
-
+  def init(:ok) do
+    {:ok, {0,[]}} #{hashId, neighborMap} , {hashId, neighborMap}
+  end
 end
 
 TapestrySimulator.main(System.argv())
